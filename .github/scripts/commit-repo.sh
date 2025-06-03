94f90176779ab95fb9fd34206f953aa0b8524d44
ZhendongWu <30779939+AlphaBoom@users.noreply.github.com>
2025-06-03 11:49:18 +0800

    Initial release

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,8 +2,8 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore ../main/repo/ .
-git config --global user.email "177773202+Kohi-den-Bot@users.noreply.github.com"
-git config --global user.name "Kohi-den-Bot"
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
@@ -11,7 +11,7 @@ if [ -n "$(git status --porcelain)" ]; then
     git push
 
     # Purge cached index on jsDelivr
-    curl https://purge.jsdelivr.net/gh/aniyomiorg/aniyomi-extensions@repo/index.min.json
+#    curl https://purge.jsdelivr.net/gh/aniyomiorg/aniyomi-extensions@repo/index.min.json
 else
     echo "No changes to commit"
 fi
